51ff991191728406396ff366b22a68b11c5ca412
Luis Miguel <lmarenast@unal.edu.co>
2021-04-27 15:56:12 -0500

    Probandooo

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-basics
-Course Django for everybody Coursera
--- Web Application Technologies and Django/Semana2/mysite/polls/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world. a0e60a13 is the polls index.")
